4f6e697579e227cd286b056c939aae2cec920316
Niko E. Rozo E <niko.rozo92@outlook.com>
2020-07-11 14:24:37 -0500

    Revisión de Variables en el AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cliente-app';
+  title = 'Modulo de Facturación';
+
+  version = 'Angular 10';
+  author = 'Nicolás E. Rozo E.';
 }
