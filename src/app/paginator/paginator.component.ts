b20f0980ad24bfc7fd25081e79ac159e79922e83
Niko E. Rozo E <niko.rozo92@outlook.com>
2020-07-22 21:01:44 -0500

    Paginador por Rango, para evitar desbordamiento

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
@@ -11,12 +11,37 @@ export class PaginatorComponent implements OnInit {
 
   pages: number[];
 
+  desde: number;
+  hasta: number;
+
   constructor() { }
 
   ngOnInit(): void {
-    this.pages = new Array(this.paginador.totalPages).fill(0).map(
-      (valor, indice) => indice + 1
-    );
+    this.initPaginator();
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnChanges(changes: SimpleChanges): void {
+    const paginadorActual = changes.paginador;
+
+    if (paginadorActual.previousValue) {
+      this.initPaginator();
+    }
+  }
+
+  private initPaginator(): void {
+    this.desde = Math.min(Math.max(1, this.paginador.number - 4), this.paginador.totalPages - 5);
+    this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 4), 6);
+
+    if (this.paginador.totalPages > 5) {
+      this.pages = new Array(this.hasta - this.desde + 1).fill(0).map(
+        (valor, indice) => indice + this.desde
+        );
+    } else {
+      this.pages = new Array(this.paginador.totalPages).fill(0).map(
+        (valor, indice) => indice + 1
+        );
+    }
   }
 
 }
